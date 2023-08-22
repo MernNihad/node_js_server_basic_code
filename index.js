32b2d8abb53af84b581664bdcc1605f62d6a68e7
NIHAD O T <mernnihadsoft@gmail.com>
2023-08-22 12:31:25 +0530

    nodejs server code

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    console.log(req.url);
+
+    if(req.url == '/home'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('Home!');
+
+    }else{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('Hello World!');
+    }
+
+}).listen(8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
